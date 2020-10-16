b6263e8ed39fda9144c31ca5654b80f395ba76a2
Rich Smith <rich@zenofprogramming.com>
2020-10-16 09:47:25 -0400

    CookieGram HelloWorld
    Built SpringBoot app and added dependencies: 
    
     - Spring Web Services
     - ThymeLeaf
     - Lomboc
    
    Created get mapping to /easteregg which returns hello world message

---
+++ src/test/java/com/zenoprogramming/com/cookiegram/CookieGramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenoprogramming.com.cookiegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
