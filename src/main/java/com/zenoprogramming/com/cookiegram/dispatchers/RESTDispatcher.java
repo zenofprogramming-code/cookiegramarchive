0fcbd99ef85ff16292d4ef5862fcbb7f991c6bd1
Rich Smith <rich@zenofprogramming.com>
2020-10-21 09:01:19 -0400

    Cleaned up cookieorder form with some Bootstrap and css
    Nothing major, just a quick clean up to make it look presentable in a demo.  
    Fixed minor but in RESTDispatcher to allow return messages from the OrderRequestResponse object to be displayed

--- src/main/java/com/zenoprogramming/com/cookiegram/dispatchers/RESTDispatcher.java
+++ src/main/java/com/zenoprogramming/com/cookiegram/dispatchers/RESTDispatcher.java
@@ -2,6 +2,7 @@ package com.zenoprogramming.com.cookiegram.dispatchers;
 import com.zenoprogramming.com.cookiegram.dto.OrderRequest;
 import com.zenoprogramming.com.cookiegram.dto.OrderRequestResponse;
 import com.zenoprogramming.com.cookiegram.usecasemanagers.OrderManager;
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -22,7 +23,8 @@ public class RESTDispatcher
    @RequestMapping("/ordercookieform")
    public String orderCookieForm (Model model)
    {
-//      model.addAttribute(new OrderRequest("", "", "", "", "", "", "", "", LocalDate.now()));
+      model.addAttribute(new OrderRequest("", "", "", "", "", "", "", "", LocalDate.now()));
+      model.addAttribute("message", "Hurray!  Our chefs are very exited and can't wait to start baking!  Please fill out this form:");
       return "ordercookieform.html";
    }
 
@@ -31,7 +33,7 @@ public class RESTDispatcher
    {
       OrderRequestResponse requestResponse = new OrderManager().createOrder(orderRequest);
       model.addAttribute("response", requestResponse);
-
+      model.addAttribute("message", requestResponse.getReturnMessageToUser());
       return requestResponse.getHtmlPage();
 
    }
--- src/test/java/com/zenoprogramming/com/cookiegram/CookieGramApplicationTests.java
+++ src/test/java/com/zenoprogramming/com/cookiegram/CookieGramApplicationTests.java
@@ -1,13 +1,30 @@
 package com.zenoprogramming.com.cookiegram;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class CookieGramApplicationTests {
+@AutoConfigureMockMvc
+class CookieGramApplicationTests
+{
+   @Autowired
+   private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
-	}
+//   @Test
+//   void contextLoads ()
+//   {
+//   }
+
+//   @Test
+//   public void testOrderRequestUseCase ()
+//   {
+//      this.mockMvc.perform(get("/orderrequest"))
+//          .andExpect(status().isOk());
+//
+//
+//   }
+
 
 }
